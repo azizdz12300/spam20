a33200d433845be885ab03ed22ea423f14c5c707
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2018-11-23 10:28:26 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const other = new Discord.Client();
+
+other.on('ready', () => {
+  console.log(`Logged in as ${other.user.tag}!`);
+});
+
+other.on('ready', async () => {
+      let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
+  setInterval(() => {
+other.channels.get("507653890205024258").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},300);
+});
+
+other.login("NTEyNjg4ODIxMDA4MzM0ODUw.DtlRwg.h6oj-Ui9yJfZ5gDP1frDPUiw-vw")
---
+++ bot4.js
@@ -0,0 +1,16 @@
+other.login("")
+const Discord = require('discord.js');
+const other = new Discord.Client();
+
+other.on('ready', () => {
+  console.log(`Logged in as ${other.user.tag}!`);
+});
+
+other.on('ready', async () => {
+      let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
+  setInterval(() => {
+other.channels.get("513397023567773707").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+},300);
+});
+
+other.login("NDc5NjAzMTIwNDgyMTU2NTU4.DtHeyA.UyFXi3FEpYEZEc1v09mBFYscfm0")
